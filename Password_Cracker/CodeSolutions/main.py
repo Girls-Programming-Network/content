cca27e24cd8b4d542dd8e33ae7e96d099609a3c6
Cat Bedwin <caitlin.bedwin@students.mq.edu.au>
2024-09-26 19:01:33 +1000

    PasswordCracker Code solutions added

---
+++ Password_Cracker/CodeSolutions/main.py
@@ -0,0 +1,24 @@
+#Password cracker workbook 1 solution 
+
+#Hashing a password and comparing it to the input
+import hashlib
+
+#The following section gets deleted by the students - it finds the hashed version of the passphrase
+##correct = "The ship sails at midnight"
+##correct_encoded = correct.encode()
+##print(type(correct_encoded))
+##correct_hashed = hashlib.md5(correct_encoded).digest()
+##print(correct_hashed)
+
+#Note: The correct passphrase is "The ship sails at midnight"
+correct_hashed = b"\xcc\xd6R\x16\xb9\x1bP~lK\x01\x0e\x063\x10\xec"
+
+guess = input("What is the passphrase? ")
+guess_encoded = guess.encode()
+guess_hashed = hashlib.md5(guess_encoded).digest()
+
+#if the hashed versions are the same, then the passphrases must have been the same
+if guess_hashed == correct_hashed:
+    print("Welcome to the club!")
+else:
+    print("Go away!")
